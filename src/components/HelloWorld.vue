cd74474dbeaad05a2760adee569731ce41fec614
Anas Alnagar <35266228+onisEg@users.noreply.github.com>
2023-01-17 00:40:18 +0200

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <form action="">
+      <input :type="fieldType" placeholder="Type your password">
+      <button @click.prevent="switchField">Show/Hide password</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HelloWorld',
+  methods: {
+    switchField() {
+      this.fieldType = this.fieldType === "password" ? "text" : "password";
+    }
+  }
+  ,
+  data() {
+    return {
+      fieldType: "password",
+      
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+input , button {
+  margin: 10px;
+  padding: 10px 9px;
+  border-radius: 5px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
